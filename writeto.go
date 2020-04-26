4a5f8da9193458d208ec95d2b4ed69eee1cdf2c7
aeneasr <aeneas@ory.sh>
2020-04-26 17:30:20 +0200

    fix: do not panic in writeBody

--- writeto.go
+++ writeto.go
@@ -258,6 +258,11 @@ func (w *messageWriter) writeHeaders(h map[string][]string) {
 }
 
 func (w *messageWriter) writeBody(f func(io.Writer) error, enc Encoding) {
+	if f == nil {
+		w.err = errors.New("writeBody: expected writer to be defined but got nil")
+		return
+	}
+
 	var subWriter io.Writer
 	if w.depth == 0 {
 		w.writeString("\r\n")
